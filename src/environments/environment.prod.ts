cf317219a7634d15d7b7efa7a0e0675f0402f670
chai-v <chaitanya.vengali@gmail.com>
2023-12-07 11:27:12 +0530

    Adding existing files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  endpoint: 'https://treasurehunt.chaitanya-sai-v.repl.co',
+  testEndPoint: 'https://TreasureHuntchai.chaitanya-sai-v.repl.co'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  endpoint: 'https://treasurehunt.chaitanya-sai-v.repl.co',
+  testEndPoint: 'https://TreasureHuntchai.chaitanya-sai-v.repl.co'
+};
+
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
